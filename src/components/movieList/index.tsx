d13c180643a54347a08979008d2a6552b2e980b5
poraif <peadar.oraif@gmail.com>
2024-06-12 23:32:26 +0200

    Added routing and live data from API.

--- src/components/movieList/index.tsx
+++ src/components/movieList/index.tsx
@@ -4,6 +4,7 @@ import Grid from "@mui/material/Grid";
 import { BaseMovieListProps } from "../../types/interfaces";
 
 const MovieList: React.FC<BaseMovieListProps> = ({movies}) => {
+  // eslint-disable-next-line prefer-const
   let movieCards = movies.map((m) => (
     <Grid key={m.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
       <Movie key={m.id} {...m} />
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,50 +1,52 @@
 export interface BaseMovieProps {
-    title: string;
-    budget: number;
-    homepage: string | undefined;
+  title: string;
+  budget: number;
+  homepage: string | undefined;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  popularity: number;
+  poster_path?: string;
+  tagline: string;
+  runtime: number;
+  revenue: number;
+  vote_count: number;
+  favourite?: boolean;
+  genre_ids?: number[];
+}
+
+export interface BaseMovieListProps { 
+  movies: BaseMovieProps[];
+}  
+
+export interface MovieDetailsProps extends BaseMovieProps {
+  genres: {
     id: number;
-    imdb_id: string;
-    original_language: string;
-    overview: string;
-    release_date: string;
-    vote_average: number;
-    popularity: number;
-    poster_path?: string;
-    tagline: string;
-    runtime: number;
-    revenue: number;
-    vote_count: number;
-    favourite?: boolean;
-  }
-
-  export interface MovieDetailsProps extends BaseMovieProps {
-    genres: {
-      id: number;
-      name: string;
-    }[];
-    production_countries: {
-      iso_3166_1: string;
-      name: string;
-    }[];
-  }
-
-  export interface BaseMovieListProps { 
-    movies: BaseMovie[];
-  }   
+    name: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+}
 
 
+export interface MovieImage {
+  file_path: string;
+  aspect_ratio?: number; //some props are optional...
+  height?: number;
+  iso_639_1?: string;
+  vote_average?: number;
+  vote_count?: number;
+  width?: number;
+}
 
-  export interface MovieImage {
-    file_path: string;
-    aspect_ratio?: number; //some props are optional...
-    height?: number;
-    iso_639_1?: string;
-    vote_average?: number;
-    vote_count?: number;
-    width?: number;
-  }
+export interface MoviePageProps {
+  movie: MovieDetailsProps;
+  images: MovieImage[];
+}
 
-  export interface MoviePageProps {
-    movie: MovieDetailsProps;
-    images: MovieImage[];
-  }
+export type FilterOption = "title" | "genre";
