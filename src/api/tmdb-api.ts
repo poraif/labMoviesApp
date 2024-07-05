8e84447c792d5019c9f2c54e04894a1382401ae8
poraif <peadar.oraif@gmail.com>
2024-07-05 15:49:31 +0200

    popular movies api fetch added

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -13,10 +13,23 @@ export const getMovies = () => {
 
 export const getUpcomingMovies = () => {
   return fetch(
-    `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&include_adult=false&page=1`
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
   ).then((response) => {
     if (!response.ok)
       throw new Error(`Unable to fetch upcoming movies. Response status: ${response.status}`);
+    return response.json();
+  })
+    .catch((error) => {
+      throw error
+    });
+};
+
+export const getPopularMovies = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+  ).then((response) => {
+    if (!response.ok)
+      throw new Error(`Unable to fetch popular movies. Response status: ${response.status}`);
     return response.json();
   })
     .catch((error) => {
